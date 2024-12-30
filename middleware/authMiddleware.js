7fa38aa15ebb17eb0b9adef072d2ef3e5ab1edaa
geniusrks1 <gmrohitkumar@gmail.com>
2024-12-30 07:06:28 +0530

    fixing middileware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,9 +1,7 @@
-// middleware/authMiddleware.js
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const dotenv = require("dotenv");
 dotenv.config();
-
 
 const authMiddleware = async (req, res, next) => {
   try {
@@ -11,56 +9,57 @@ const authMiddleware = async (req, res, next) => {
 
     if (!Authorization) {
       return res.status(401).json({
-        status: 'error',
-        message: 'Please login to access this route',
+        status: "error",
+        message: "Please login to access this route",
       });
     }
 
-  
-   
-    const token = Authorization;
+    
+    const token = Authorization.startsWith("Bearer ")
+      ? Authorization.slice(7) 
+      : Authorization;
+
     if (!token) {
-      console.log(token);
       return res.status(401).json({
-        status: 'error',
-        message: 'Invalid token format. Please login to access this route.',
+        status: "error",
+        message: "Invalid token format. Please login to access this route.",
       });
     }
 
     // Verify the token
     let decoded;
     try {
-      decoded = jwt.verify(token, process.env.JWT_SECRET_KEY); 
+      decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     } catch (err) {
-      if (err.name === 'TokenExpiredError') {
+      if (err.name === "TokenExpiredError") {
         return res.status(401).json({
-          status: 'error',
-          message: 'Token has expired. Please login again.',
+          status: "error",
+          message: "Token has expired. Please login again.",
         });
       }
       return res.status(401).json({
-        status: 'error',
-        message: 'Invalid token. Please login again.',
+        status: "error",
+        message: "Invalid token. Please login again.",
       });
     }
 
-    // Find user associated with the decoded ID
+    
     const user = await User.findOne({ _id: decoded.id });
     if (!user) {
       return res.status(401).json({
-        status: 'error',
-        message: 'User does not exist. Please login again.',
+        status: "error",
+        message: "User does not exist. Please login again.",
       });
     }
 
-    // Attach user to the request object
+ 
     req.user = user;
     next();
   } catch (error) {
-    console.error('Error in authMiddleware:', error);
+    console.error("Error in authMiddleware:", error);
     res.status(500).json({
-      status: 'error',
-      message: error.message || 'An error occurred. Please try again.',
+      status: "error",
+      message: error.message || "An error occurred. Please try again.",
     });
   }
 };
